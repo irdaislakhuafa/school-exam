fe7b8625265d01f18d5e67969d1940202cf7314f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-06-01 23:35:04 +0700

    added tiny island 2 and 3

--- application/views/student/login.php
+++ application/views/student/login.php
@@ -6,7 +6,7 @@ $this->basic->header("Login Siswa");
 $this->basic->headerBootstrap();
 ?>
 
-<body>
+<body class="default-backgound-color">
     <!-- <div class="limiter bg-student-login">
         <div class="position-relative m-4">
             <div class="wrap-login100">
@@ -61,45 +61,61 @@ $this->basic->headerBootstrap();
         </div>
     </div> -->
 
-    <div class="position-relative py-2 h-100 pt-5 px-4 align-content-center default-backgound-color">
+    <div class="position-relative py-2 h-100 pt-5 px-4 align-content-center">
 
         <!-- tidy island 1 -->
-        <img src="assets/images/tiny-island-1.png" class="tiny-island ms-3" alt="island 1">
-
-        <h1 class="text-uppercase font-weight-bold mb-5 fs-30 text-center text-white">halaman login siswa</h1>
-        <!-- start card -->
-        <div class="card p-4 shadow  fs-24 start-50 rounded-4 translate-middle-x " style="width: 40rem; height: max-content">
-            <div class="card-body">
-                <!-- name of student -->
-                <div class="mb-3">
-                    <label for="name" class="form-label">Nama Siswa</label>
-                    <div class="input-group mb-3">
-                        <input id="name" name="name" type="text" class="form-control rounded-3" placeholder="Masukan Nama" aria-label="Masukan Nama" aria-describedby="basic-addon2">
-                    </div>
-                </div>
-                <!-- absen code of student -->
-                <div class="mb-3">
-                    <label for="name" class="form-label">No. Absen</label>
-                    <div class="input-group mb-3">
-                        <input id="name" name="name" type="text" class="form-control rounded-3" placeholder="Masukan No. Absen" aria-label="Masukan No. Absen" aria-describedby="basic-addon2">
-                    </div>
-                </div>
-                <!-- class code of student -->
-                <div class="mb-3">
-                    <label for="name" class="form-label">Kode</label>
-                    <div class="input-group mb-3">
-                        <input id="name" name="name" type="text" class="form-control rounded-3" placeholder="Masukan Kode" aria-label="Masukan Kode" aria-describedby="basic-addon2">
-                    </div>
-                </div>
-            </div>
-
-            <!-- login button -->
-            <div class="d-flex align-items-center justify-content-center">
-                <button type="submit" class="btn btn-info w-25">Masuk</button>
-            </div>
+        <div>
+            <img src="assets/images/tiny-island-1.png" class="tiny-island ms-3" alt="island 1">
         </div>
-        <!-- end card -->
+
+        <!-- tidy island 2 -->
+        <div class="position-absolute offset-10 pt-5 mt-5">
+            <img src="assets/images/tiny-island-2.png" class="tiny-island" alt="island 1">
+        </div>
+
+        <!-- tidy island 3 -->
+        <div class="position-absolute top-50 offset-1 pt-5 mt-5">
+            <img src="assets/images/tiny-island-3.png" class="tiny-island" alt="island 1">
+        </div>
+
+        <!-- start login page -->
+        <div class="position-absolute top-0 start-50 translate-middle-x mt-5">
+            <h1 class="text-uppercase font-weight-bold mb-5 fs-30 text-center text-white">halaman login siswa</h1>
+            <!-- start card -->
+            <div class="card p-4 shadow  fs-24 rounded-4 " style="width: 40rem; height: max-content">
+                <div class="card-body">
+                    <!-- name of student -->
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nama Siswa</label>
+                        <div class="input-group mb-3">
+                            <input id="name" name="name" type="text" class="form-control rounded-3" placeholder="Masukan Nama" aria-label="Masukan Nama" aria-describedby="basic-addon2">
+                        </div>
+                    </div>
+                    <!-- absen code of student -->
+                    <div class="mb-3">
+                        <label for="name" class="form-label">No. Absen</label>
+                        <div class="input-group mb-3">
+                            <input id="name" name="name" type="text" class="form-control rounded-3" placeholder="Masukan No. Absen" aria-label="Masukan No. Absen" aria-describedby="basic-addon2">
+                        </div>
+                    </div>
+                    <!-- class code of student -->
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Kode</label>
+                        <div class="input-group mb-3">
+                            <input id="name" name="name" type="text" class="form-control rounded-3" placeholder="Masukan Kode" aria-label="Masukan Kode" aria-describedby="basic-addon2">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- login button -->
+                <div class="d-flex align-items-center justify-content-center">
+                    <button type="submit" class="btn btn-info w-25">Masuk</button>
+                </div>
+            </div>
+            <!-- end card -->
+        </div>
     </div>
+    <!-- end login page -->
 
     <!-- footer scripts -->
     <?php
