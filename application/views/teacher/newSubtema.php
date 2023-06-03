11f501df5d174f4f5c0d71a5565601814ced6ccc
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-06-03 20:23:52 +0700

    added todo to add 5 question and remove unused code

--- application/views/teacher/newSubtema.php
+++ application/views/teacher/newSubtema.php
@@ -11,56 +11,69 @@ $this->basic->headerBootstrap();
         <!-- show tiny islands -->
         <?php $this->island->showTinyIslands() ?>
 
-        <!-- start login page -->
-        <div class="position-absolute top-0 start-50 translate-middle-x mt-5">
-            <!-- start card -->
-            <div class="card p-4 shadow  fs-24 rounded-4 " style="width: 80rem; height: max-content; min-height: 50rem;">
-                <!-- start card body -->
-                <div class="card-body">
+        <!-- TODO: soalnya ada 5 -->
+        <!-- TODO: save soal -->
+        <form action="" method="post">
+            <!-- start subtema/soal page -->
+            <div class="position-absolute top-0 start-50 translate-middle-x mt-5">
+                <!-- start card -->
+                <div class="card p-4 shadow mb-5 fs-24 rounded-4 " style="width: 80rem; height: max-content; min-height: 50rem;">
+                    <!-- start card body -->
+                    <div class="card-body">
 
-                    <div class="">
-                        <!-- teacher avatar here -->
-                        <?php $this->teacher->avatarProfileImg() ?>
+                        <div class="">
+                            <!-- teacher avatar here -->
+                            <?php $this->teacher->avatarProfileImg() ?>
 
-                        <!-- TODO: added functionality here -->
+                            <!-- TODO: added functionality here -->
 
-                        <!-- start class -->
-                        <div class="col-10 float-start w-100">
-                            <div class="ms-5 me-5">
-                                <div class="mb-3 font-weight-bold">Subtema <?= $subtemaNumber ?></div>
+                            <!-- start class -->
+                            <div class="col-10 float-start w-100">
+                                <div class="ms-5 me-5">
+                                    <div class="mb-3 font-weight-bold">Subtema <?= $subtemaNumber ?></div>
 
-                                <!-- start input subtema -->
-                                <div class="">
-                                    <!-- materi -->
-                                    <div class="mb-3">
-                                        <label for="exampleFormControlTextarea1" class="form-label">Materi</label>
-                                        <textarea class="form-control shadow rounded-2" rows="13" style="background-color: #F5F5F5 !important;" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                    <!-- start input subtema -->
+                                    <div class="">
+                                        <!-- materi -->
+                                        <div class="mb-3">
+                                            <label for="exampleFormControlTextarea1" class="form-label">Materi</label>
+                                            <textarea class="form-control shadow rounded-2" rows="13" style="background-color: #F5F5F5 !important;" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                            <div class="row">
+                                                <?php for ($i = 1; $i <= 3; $i++) { ?>
+                                                    <div class="col-4 w-100 me-5 fs-16">
+                                                        <input type="file" class=" mb-2 mt-2 shadow" name="image<?= $i ?>" id="">
+                                                        <textarea name="image<?= $i ?>Description" id="" cols="20" rows="2" class="form-control shadow rounded-2"></textarea>
+                                                    </div>
+                                                <?php } ?>
+                                            </div>
+                                        </div>
+                                        <!-- soal -->
+                                        <div class="mb-3">
+                                            <label for="exampleFormControlTextarea1" class="form-label">Soal</label>
+                                            <textarea class="form-control shadow rounded-2" rows="8" style="background-color: #F5F5F5 !important;" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                        </div>
                                     </div>
-                                    <!-- soal -->
-                                    <div class="mb-3">
-                                        <label for="exampleFormControlTextarea1" class="form-label">Soal</label>
-                                        <textarea class="form-control shadow rounded-2" rows="8" style="background-color: #F5F5F5 !important;" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                    </div>
+                                    <!-- end input subtema -->
+
                                 </div>
-                                <!-- end input subtema -->
-
                             </div>
+                            <!-- end class -->
                         </div>
-                        <!-- end class -->
+                    </div>
+                    <!-- end card body -->
+
+                    <!-- next button -->
+                    <div class="d-flex justify-content-between">
+                        <button id="back" type="submit" class="btn shadow bg-white ps-5 pe-5">Kembali</button>
+                        <button id="save" type="submit" class="btn shadow bg-white ps-5 pe-5">Simpan</button>
                     </div>
                 </div>
-                <!-- end card body -->
-
-                <!-- next button -->
-                <div class="d-flex justify-content-between">
-                    <button id="back" type="submit" class="btn shadow bg-white ps-5 pe-5">Kembali</button>
-                    <button id="save" type="submit" class="btn shadow bg-white ps-5 pe-5">Simpan</button>
-                </div>
+                <!-- end card -->
             </div>
-            <!-- end card -->
-        </div>
+        </form>
+
     </div>
-    <!-- end login page -->
+    <!-- end subtema/soal page -->
 
     <script>
         // back
