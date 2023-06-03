ed43a587420ad407656ff742922793d164ca75cf
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-06-03 12:58:48 +0700

    added title  prefix "School Exam"

--- application/libraries/Basic.php
+++ application/libraries/Basic.php
@@ -5,7 +5,7 @@ class Basic
     { ?>
 
         <head>
-            <title><?= $title ?></title>
+            <title>School Exam | <?= $title ?></title>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <!--===============================================================================================-->
