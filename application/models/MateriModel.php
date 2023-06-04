4ed274ebf27a496222626df80047bc1e27053111
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-06-04 21:29:04 +0700

    added method insert, get and getList in MateriModel

---
+++ application/models/MateriModel.php
@@ -0,0 +1,30 @@
+<?php
+
+class MateriModel extends CI_Model
+{
+    private static $TABLE = 'materi';
+    public function insert($materi)
+    {
+        $materi['id'] = $this->random->generateUUID();
+        return $this->db
+            ->insert(MateriModel::$TABLE, $materi);
+    }
+
+    // result is list of object
+    public function getList($materi = array())
+    {
+        $result = $this->db
+            ->where($materi)
+            ->get(MateriModel::$TABLE);
+        return $result->result_object();
+    }
+
+    // result is one object
+    public function get($materi = array())
+    {
+        $result = $this->db
+            ->where($materi)
+            ->get(MateriModel::$TABLE);
+        return $result->row_object();
+    }
+}
--- application/models/StudentModel.php
+++ application/models/StudentModel.php
@@ -6,7 +6,6 @@ class StudentModel extends CI_Model
     public function getListByClassId($classId)
     {
         $result = $this->db->where(array("classId" => $classId))->get(StudentModel::$TABLE);
-        // var_dump($this->db->last_query());
         return $result->result_array();
     }
 }
